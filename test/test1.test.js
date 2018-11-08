bd9dac8d7ab3a3ac747a19ad83de31414b0e865d
AmbroseNTK <nguyentuankiet23031998@gmail.com>
2018-11-08 15:06:12 +0700

    no message

---
+++ test/test1.test.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var chai_1 = require("chai");
+describe("Test 1", function () {
+    it("test 1", function () {
+        chai_1.expect(1).to.equal(1);
+    });
+});
---
+++ test/test1.test.ts
@@ -0,0 +1,7 @@
+import {expect} from 'chai'
+
+describe("Test 1",()=>{
+    it("test 1",()=>{
+        expect(1).to.equal(1);
+    });
+})
